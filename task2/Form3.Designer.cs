a72ad17a6f78fd09cb4fe04571da460b72c524ba
Ekaterina0801 <ds.katrin@mail.ru>
2023-09-13 09:18:27 +0300

    add lab2

---
+++ task2/Form3.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace lab2
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pbOriginal = new PictureBox();
+            numHue = new TrackBar();
+            numSaturation = new TrackBar();
+            numValue = new TrackBar();
+            btnProcess = new Button();
+            btnSave = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pbOriginal).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numHue).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numSaturation).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numValue).BeginInit();
+            SuspendLayout();
+            // 
+            // pbOriginal
+            // 
+            pbOriginal.Location = new Point(69, 59);
+            pbOriginal.Name = "pbOriginal";
+            pbOriginal.Size = new Size(706, 329);
+            pbOriginal.TabIndex = 0;
+            pbOriginal.TabStop = false;
+            // 
+            // numHue
+            // 
+            numHue.Location = new Point(886, 59);
+            numHue.Maximum = 180;
+            numHue.Minimum = -180;
+            numHue.Name = "numHue";
+            numHue.Size = new Size(437, 90);
+            numHue.TabIndex = 2;
+            // 
+            // numSaturation
+            // 
+            numSaturation.Location = new Point(895, 209);
+            numSaturation.Maximum = 100;
+            numSaturation.Minimum = -100;
+            numSaturation.Name = "numSaturation";
+            numSaturation.Size = new Size(437, 90);
+            numSaturation.TabIndex = 3;
+            // 
+            // numValue
+            // 
+            numValue.Location = new Point(886, 325);
+            numValue.Maximum = 100;
+            numValue.Minimum = -100;
+            numValue.Name = "numValue";
+            numValue.Size = new Size(437, 90);
+            numValue.TabIndex = 4;
+            // 
+            // btnProcess
+            // 
+            btnProcess.Location = new Point(895, 435);
+            btnProcess.Name = "btnProcess";
+            btnProcess.Size = new Size(410, 46);
+            btnProcess.TabIndex = 5;
+            btnProcess.Text = "Обработать";
+            btnProcess.UseVisualStyleBackColor = true;
+            btnProcess.Click += btnProcess_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(895, 547);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(410, 46);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Сохранить";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(218, 435);
+            button1.Name = "button1";
+            button1.Size = new Size(410, 46);
+            button1.TabIndex = 7;
+            button1.Text = "Открыть изображение";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnOpen_Click;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1458, 664);
+            Controls.Add(button1);
+            Controls.Add(btnSave);
+            Controls.Add(btnProcess);
+            Controls.Add(numValue);
+            Controls.Add(numSaturation);
+            Controls.Add(numHue);
+            Controls.Add(pbOriginal);
+            Name = "Form3";
+            Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)pbOriginal).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numHue).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numSaturation).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numValue).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pbOriginal;
+        private TrackBar numHue;
+        private TrackBar numSaturation;
+        private TrackBar numValue;
+        private Button btnProcess;
+        private Button btnSave;
+        private Button button1;
+    }
+}
---
+++ task2/Form3.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form3 : Form
+    {
+        private Bitmap originalImage;
+        private Bitmap modifiedImage;
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.png, *.jpg, *.jpeg, *.bmp)|*.png;*.jpg;*.jpeg;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openFileDialog.FileName);
+                pbOriginal.Image = originalImage;
+                modifiedImage = new Bitmap(originalImage.Width, originalImage.Height);
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (originalImage != null)
+            {
+                for (int x = 0; x < originalImage.Width; x++)
+                {
+                    for (int y = 0; y < originalImage.Height; y++)
+                    {
+                        Color originalColor = originalImage.GetPixel(x, y);
+                        Color modifiedColor = ColorToHSV(originalColor);
+                        modifiedImage.SetPixel(x, y, modifiedColor);
+                    }
+                }
+
+                pbOriginal.Image = modifiedImage;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (modifiedImage != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Image Files (*.png, *.jpg, *.jpeg, *.bmp)|*.png;*.jpg;*.jpeg;*.bmp";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    modifiedImage.Save(saveFileDialog.FileName, ImageFormat.Png);
+                }
+            }
+        }
+
+        private Color ColorToHSV(Color originalColor)
+        {
+            float hue, saturation, value;
+            hue = originalColor.GetHue();
+            saturation = originalColor.GetSaturation();
+            value = originalColor.GetBrightness();
+
+            hue += (float)numHue.Value;
+            saturation += (float)numSaturation.Value / 100;
+            value += (float)numValue.Value / 100;
+
+            hue = Math.Max(0, Math.Min(360, hue));
+            saturation = Math.Max(0, Math.Min(1, saturation));
+            value = Math.Max(0, Math.Min(1, value));
+
+            return ColorFromHSV(hue, saturation, value);
+        }
+
+        private Color ColorFromHSV(float hue, float saturation, float value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            float f = hue / 60 - (float)Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+    }
+}
