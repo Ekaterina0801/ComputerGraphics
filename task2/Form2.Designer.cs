e9c8b4ee20ab248c00ffb9d9ccf98e5cf6e9790e
vadimka-8888 <73032993+vadimka-8888@users.noreply.github.com>
2023-09-20 09:40:22 +0300

    add second form

--- task2/Form2.Designer.cs
+++ task2/Form2.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            originalPictureBox = new PictureBox();
+            redPictureBox = new PictureBox();
+            greenPictureBox = new PictureBox();
+            bluePictureBox = new PictureBox();
+            openButton = new Button();
+            chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)originalPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)redPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)greenPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bluePictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart).BeginInit();
+            SuspendLayout();
+            // 
+            // originalPictureBox
+            // 
+            originalPictureBox.Location = new Point(12, 42);
+            originalPictureBox.Name = "originalPictureBox";
+            originalPictureBox.Size = new Size(415, 327);
+            originalPictureBox.TabIndex = 0;
+            originalPictureBox.TabStop = false;
+            // 
+            // redPictureBox
+            // 
+            redPictureBox.Location = new Point(475, 42);
+            redPictureBox.Name = "redPictureBox";
+            redPictureBox.Size = new Size(415, 327);
+            redPictureBox.TabIndex = 1;
+            redPictureBox.TabStop = false;
+            // 
+            // greenPictureBox
+            // 
+            greenPictureBox.Location = new Point(951, 42);
+            greenPictureBox.Name = "greenPictureBox";
+            greenPictureBox.Size = new Size(415, 327);
+            greenPictureBox.TabIndex = 2;
+            greenPictureBox.TabStop = false;
+            // 
+            // bluePictureBox
+            // 
+            bluePictureBox.Location = new Point(1425, 42);
+            bluePictureBox.Name = "bluePictureBox";
+            bluePictureBox.Size = new Size(415, 327);
+            bluePictureBox.TabIndex = 3;
+            bluePictureBox.TabStop = false;
+            // 
+            // openButton
+            // 
+            openButton.Location = new Point(748, 904);
+            openButton.Name = "openButton";
+            openButton.Size = new Size(429, 74);
+            openButton.TabIndex = 4;
+            openButton.Text = "Открыть изображение";
+            openButton.UseVisualStyleBackColor = true;
+            openButton.Click += openButton_Click;
+            // 
+            // chart
+            // 
+            chartArea2.Name = "ChartArea1";
+            chart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            chart.Legends.Add(legend2);
+            chart.Location = new Point(465, 428);
+            chart.Name = "chart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            chart.Series.Add(series2);
+            chart.Size = new Size(1044, 448);
+            chart.TabIndex = 5;
+            chart.Text = "chart1";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1917, 1008);
+            Controls.Add(chart);
+            Controls.Add(openButton);
+            Controls.Add(bluePictureBox);
+            Controls.Add(greenPictureBox);
+            Controls.Add(redPictureBox);
+            Controls.Add(originalPictureBox);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)originalPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)redPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)greenPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bluePictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox originalPictureBox;
+        private PictureBox redPictureBox;
+        private PictureBox greenPictureBox;
+        private PictureBox bluePictureBox;
+        private Button openButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
     }
 }
--- task2/Form2.cs
+++ task2/Form2.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace lab2
 {
     public partial class Form2 : Form
@@ -15,6 +20,100 @@ namespace lab2
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Load the selected image file
+                Bitmap image = new Bitmap(openFileDialog.FileName);
+
+                // Display the original image
+                originalPictureBox.Image = image;
+
+                // Create separate images for each color channel
+                Bitmap redImage = new Bitmap(image.Width, image.Height);
+                Bitmap greenImage = new Bitmap(image.Width, image.Height);
+                Bitmap blueImage = new Bitmap(image.Width, image.Height);
+
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Color pixelColor = image.GetPixel(x, y);
+
+                        // Get the red component of the pixel color
+                        int red = pixelColor.R;
+                        // Set the pixel color in the red image
+                        redImage.SetPixel(x, y, Color.FromArgb(red, 0, 0));
+
+                        // Get the green component of the pixel color
+                        int green = pixelColor.G;
+                        // Set the pixel color in the green image
+                        greenImage.SetPixel(x, y, Color.FromArgb(0, green, 0));
+
+                        // Get the blue component of the pixel color
+                        int blue = pixelColor.B;
+                        // Set the pixel color in the blue image
+                        blueImage.SetPixel(x, y, Color.FromArgb(0, 0, blue));
+                    }
+                }
+
+                // Display the separate color channel images
+                redPictureBox.Image = redImage;
+                greenPictureBox.Image = greenImage;
+                bluePictureBox.Image = blueImage;
+
+                // Build histograms for each color channel
+                int[] redHistogram = new int[256];
+                int[] greenHistogram = new int[256];
+                int[] blueHistogram = new int[256];
+
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Color pixelColor = image.GetPixel(x, y);
+
+                        // Increment the respective color channel histogram
+                        redHistogram[pixelColor.R]++;
+                        greenHistogram[pixelColor.G]++;
+                        blueHistogram[pixelColor.B]++;
+                    }
+                }
+
+                // Display the histograms using a chart control
+                chart.Series.Clear();
+                chart.ChartAreas[0].AxisX.Title = "Color Intensity";
+                chart.ChartAreas[0].AxisY.Title = "Frequency";
+
+                Series redSeries = new Series("Red");
+                redSeries.ChartType = SeriesChartType.Column;
+                for (int i = 0; i < 256; i++)
+                {
+                    redSeries.Points.AddXY(i, redHistogram[i]);
+                }
+                chart.Series.Add(redSeries);
+
+                Series greenSeries = new Series("Green");
+                greenSeries.ChartType = SeriesChartType.Column;
+                for (int i = 0; i < 256; i++)
+                {
+                    greenSeries.Points.AddXY(i, greenHistogram[i]);
+                }
+                chart.Series.Add(greenSeries);
+
+                Series blueSeries = new Series("Blue");
+                blueSeries.ChartType = SeriesChartType.Column;
+                for (int i = 0; i < 256; i++)
+                {
+                    blueSeries.Points.AddXY(i, blueHistogram[i]);
+                }
+                chart.Series.Add(blueSeries);
+            }
         }
     }
 }
