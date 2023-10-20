fa1513b0975a1c273ba63274f2ed2c9f45ef8136
vadimka-8888 <73032993+vadimka-8888@users.noreply.github.com>
2023-10-20 23:17:57 +0300

    Add: lab5, L-systems and the tree

--- task5/Form1.Designer.cs
+++ task5/Form1.Designer.cs
@@ -40,19 +40,31 @@
             this.label1 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.createBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.col_change_box = new System.Windows.Forms.CheckBox();
+            this.fort_box = new System.Windows.Forms.CheckBox();
+            this.width_box = new System.Windows.Forms.CheckBox();
+            this.col_box = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.len_box = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.intensity_box = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.inputControl.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 74);
+            this.pictureBox1.Location = new System.Drawing.Point(18, 150);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(502, 371);
+            this.pictureBox1.Size = new System.Drawing.Size(751, 751);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -60,20 +72,22 @@
             // 
             this.inputControl.Controls.Add(this.tabPage1);
             this.inputControl.Controls.Add(this.tabPage2);
-            this.inputControl.Location = new System.Drawing.Point(0, 451);
+            this.inputControl.Location = new System.Drawing.Point(800, 150);
+            this.inputControl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.inputControl.Name = "inputControl";
             this.inputControl.Padding = new System.Drawing.Point(20, 3);
             this.inputControl.SelectedIndex = 0;
-            this.inputControl.Size = new System.Drawing.Size(526, 148);
+            this.inputControl.Size = new System.Drawing.Size(461, 289);
             this.inputControl.TabIndex = 1;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.inputBox);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(518, 122);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage1.Size = new System.Drawing.Size(453, 256);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Input";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -81,19 +95,21 @@
             // inputBox
             // 
             this.inputBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.inputBox.Location = new System.Drawing.Point(3, 3);
+            this.inputBox.Location = new System.Drawing.Point(4, 5);
+            this.inputBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.inputBox.Multiline = true;
             this.inputBox.Name = "inputBox";
-            this.inputBox.Size = new System.Drawing.Size(512, 116);
+            this.inputBox.Size = new System.Drawing.Size(445, 246);
             this.inputBox.TabIndex = 0;
             // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.outputBox);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(858, 122);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage2.Size = new System.Drawing.Size(453, 256);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Output";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -101,19 +117,22 @@
             // outputBox
             // 
             this.outputBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.outputBox.Location = new System.Drawing.Point(3, 3);
+            this.outputBox.Location = new System.Drawing.Point(4, 5);
+            this.outputBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.outputBox.Multiline = true;
             this.outputBox.Name = "outputBox";
-            this.outputBox.Size = new System.Drawing.Size(852, 116);
+            this.outputBox.Size = new System.Drawing.Size(445, 246);
             this.outputBox.TabIndex = 0;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(526, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1266, 33);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -122,48 +141,148 @@
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(235, 34);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Location = new System.Drawing.Point(18, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(80, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Iterations:";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(71, 36);
+            this.numericUpDown1.Location = new System.Drawing.Point(106, 55);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 26);
             this.numericUpDown1.TabIndex = 4;
             // 
             // createBtn
             // 
-            this.createBtn.Location = new System.Drawing.Point(129, 34);
+            this.createBtn.Location = new System.Drawing.Point(185, 52);
+            this.createBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.createBtn.Name = "createBtn";
-            this.createBtn.Size = new System.Drawing.Size(96, 23);
+            this.createBtn.Size = new System.Drawing.Size(144, 35);
             this.createBtn.TabIndex = 5;
             this.createBtn.Text = "Create";
             this.createBtn.UseVisualStyleBackColor = true;
             this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.intensity_box);
+            this.groupBox1.Controls.Add(this.col_change_box);
+            this.groupBox1.Controls.Add(this.fort_box);
+            this.groupBox1.Controls.Add(this.width_box);
+            this.groupBox1.Controls.Add(this.col_box);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.len_box);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(345, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(623, 90);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // col_change_box
+            // 
+            this.col_change_box.AutoSize = true;
+            this.col_change_box.Location = new System.Drawing.Point(374, 21);
+            this.col_change_box.Name = "col_change_box";
+            this.col_change_box.Size = new System.Drawing.Size(144, 24);
+            this.col_change_box.TabIndex = 6;
+            this.col_change_box.Text = "Color Changing";
+            this.col_change_box.UseVisualStyleBackColor = true;
+            // 
+            // fort_box
+            // 
+            this.fort_box.AutoSize = true;
+            this.fort_box.Location = new System.Drawing.Point(203, 56);
+            this.fort_box.Name = "fort_box";
+            this.fort_box.Size = new System.Drawing.Size(88, 24);
+            this.fort_box.TabIndex = 5;
+            this.fort_box.Text = "Fortuity";
+            this.fort_box.UseVisualStyleBackColor = true;
+            // 
+            // width_box
+            // 
+            this.width_box.AutoSize = true;
+            this.width_box.Location = new System.Drawing.Point(203, 21);
+            this.width_box.Name = "width_box";
+            this.width_box.Size = new System.Drawing.Size(136, 24);
+            this.width_box.TabIndex = 4;
+            this.width_box.Text = "Change Width";
+            this.width_box.UseVisualStyleBackColor = true;
+            // 
+            // col_box
+            // 
+            this.col_box.Location = new System.Drawing.Point(68, 53);
+            this.col_box.Name = "col_box";
+            this.col_box.Size = new System.Drawing.Size(98, 26);
+            this.col_box.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Color";
+            // 
+            // len_box
+            // 
+            this.len_box.Location = new System.Drawing.Point(68, 19);
+            this.len_box.Name = "len_box";
+            this.len_box.Size = new System.Drawing.Size(98, 26);
+            this.len_box.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Length";
+            // 
+            // intensity_box
+            // 
+            this.intensity_box.Location = new System.Drawing.Point(517, 53);
+            this.intensity_box.Name = "intensity_box";
+            this.intensity_box.Size = new System.Drawing.Size(100, 26);
+            this.intensity_box.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(373, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Change Intensity";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 599);
+            this.ClientSize = new System.Drawing.Size(1266, 935);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.createBtn);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
@@ -171,6 +290,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "L-System";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -182,6 +302,8 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,6 +323,16 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox col_box;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox len_box;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox width_box;
+        private System.Windows.Forms.CheckBox fort_box;
+        private System.Windows.Forms.CheckBox col_change_box;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox intensity_box;
     }
 }
 
