da7d1ca1456cb0b7ee2a689513916bc868946af8
Aleksandr Kalinin <akalinin@sfedu.ru>
2023-12-02 15:35:12 +0300

    Almost ready

--- task9/task9/View3D.cs
+++ task9/task9/View3D.cs
@@ -42,18 +42,17 @@ namespace task9
         private BitmapData bitmapData;
 
         private Scene sceneView;
-        //private double Width;
-
-        //private double Height;
 
         private Vector Center;
 
         private Vector CamPosition;
         private Matrix viewProjection;
 
-        private double Width; //{ get { return sceneView.Width; } set { Width = value; } }
-        private double Height;// { get { return sceneView.Height; } set { Height = value; } }
+        //public double Width { get { if (sceneView != null) return sceneView.Width; return this.Width; } set { } }
+        //public double Height { get { if (sceneView != null) return sceneView.Height; return this.Height; } set { } }
 
+        public double Width;
+        public double Height;
         public View3D(Scene sceneView)
         {
             this.sceneView = sceneView;
@@ -63,7 +62,7 @@ namespace task9
         {
             graphics = g;
             this.viewProjection = viewProjection;
-            Width = w;
+           Width = w;
             Height = h;
             colorBuffer = new Bitmap((int)Width + 1, (int)Height + 1);
             Center = center;
@@ -259,12 +258,81 @@ namespace task9
         }
         public void DrawLine(Vertex a, Vertex b)
         {
-            var t = SpaceToClip(a.Coordinate);
-            var A = ClipToScreen(t);
-            var u = SpaceToClip(b.Coordinate);
-            var B = ClipToScreen(u);
-            if (ShouldBeDrawn(A))
-                graphics.DrawLine(new Pen(a.Color), (float)A.X, (float)A.Y, (float)B.X, (float)B.Y);
+            if (graphics == null)
+            {
+                // Преобразование координат
+                a.Coordinate = SpaceToClip(a.Coordinate);
+                b.Coordinate = SpaceToClip(b.Coordinate);
+
+                // Обрезка линии
+                if (!ClipLine(ref a, ref b))
+                    return;
+
+                // Преобразование координат
+                a.Coordinate = ClipToScreen(a.Coordinate);
+                b.Coordinate = ClipToScreen(b.Coordinate);
+
+                // Извлечение координат
+                int x0 = (int)a.Coordinate.X;
+                int y0 = (int)a.Coordinate.Y;
+                int x1 = (int)b.Coordinate.X;
+                int y1 = (int)b.Coordinate.Y;
+
+                // Вычисление разницы между координатами
+                int dx = Math.Abs(x1 - x0);
+                int dy = Math.Abs(y1 - y0);
+
+                // Определение направления
+                int sx = x0 < x1 ? 1 : -1;
+                int sy = y0 < y1 ? 1 : -1;
+
+                // Вычисление ошибки
+                int err = dx - dy;
+
+                // Инициализация текущих координат
+                int currentX = x0;
+                int currentY = y0;
+
+                // Основной цикл отрисовки
+                while (true)
+                {
+                    // Вычисление интерполированных точек
+                    double f = dx < dy ? Math.Abs(currentY - a.Coordinate.Y) / dy : Math.Abs(currentX - a.Coordinate.X) / dx;
+                    var point = Interpolate(a, b, f);
+
+                    // Установка пикселя
+                    SetPixel(currentX, currentY, point.Coordinate.Z, point.Color);
+
+                    // Проверка завершения цикла
+                    if (currentX == x1 && currentY == y1)
+                        break;
+
+                    // Вычисление новой ошибки
+                    int e2 = 2 * err;
+
+                    if (e2 > -dy)
+                    {
+                        err -= dy;
+                        currentX += sx;
+                    }
+
+                    if (e2 < dx)
+                    {
+                        err += dx;
+                        currentY += sy;
+                    }
+                }
+            }
+            else 
+            {
+                var t = SpaceToClip(a.Coordinate);
+                var A = ClipToScreen(t);
+                var u = SpaceToClip(b.Coordinate);
+                var B = ClipToScreen(u);
+                if (ShouldBeDrawn(A))
+                    graphics.DrawLine(new Pen(a.Color), (float)A.X, (float)A.Y, (float)B.X, (float)B.Y);
+            }
+
         }
 
 
@@ -327,40 +395,70 @@ namespace task9
         }
         public void DrawTriangle(Vertex a, Vertex b, Vertex c)
         {
-            Vector p1 = a.Coordinate;
-            Vector p2 = b.Coordinate;
-            Vector p3 = c.Coordinate;
-            if (CamPosition.Y < 0.5 || CamPosition.X < 0.5 || CamPosition.Z < 0.5 || CamPosition.W < 0.5)
+            if (ActiveTexture == null)
             {
-                return;
+                Color ac, bc, cc;
+                ac = bc = cc = Color.Black;
+                foreach (var lightSource in LightSources)
+                {
+                    ac = NormalizedAdd(ac, calculateBright(a, lightSource));
+                    bc = NormalizedAdd(bc, calculateBright(b, lightSource));
+                    cc = NormalizedAdd(cc, calculateBright(c, lightSource));
+                }
+                a.Color = ac;
+                b.Color = bc;
+                c.Color = cc;
+                a.Coordinate = SpaceToClip(a.Coordinate);
+                b.Coordinate = SpaceToClip(b.Coordinate);
+                c.Coordinate = SpaceToClip(c.Coordinate);
+                var vertices = ClipTriangle(a, b, c);
+
+                if (vertices == null)
+                    return;
+
+                for (int i = 0; i < vertices.Count; ++i)
+                    vertices[i].Coordinate = ClipToScreen(vertices[i].Coordinate);
+                DrawPolygonInternal2(vertices);
+            }
+            else
+            {
+                Vector p1 = a.Coordinate;
+                Vector p2 = b.Coordinate;
+                Vector p3 = c.Coordinate;
+                if (CamPosition.Y < 0.5 || CamPosition.X < 0.5 || CamPosition.Z < 0.5 || CamPosition.W < 0.5)
+                {
+                    return;
+                }
+
+                double[,] matrix = new double[2, 3];
+                matrix[0, 0] = p2.X - p1.X;
+                matrix[0, 1] = p2.Y - p1.Y;
+                matrix[0, 2] = p2.Z - p1.Z;
+                matrix[1, 0] = p3.X - p1.X;
+                matrix[1, 1] = p3.Y - p1.Y;
+                matrix[1, 2] = p3.Z - p1.Z;
+
+                double ni = matrix[0, 1] * matrix[1, 2] - matrix[0, 2] * matrix[1, 1];
+                double nj = matrix[0, 2] * matrix[1, 0] - matrix[0, 0] * matrix[1, 2];
+                double nk = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+                double d = -(ni * p1.X + nj * p1.Y + nk * p1.Z);
+
+                Vector pp = new Vector(p1.X + ni, p1.Y + nj, p1.Z + nk);
+                double val1 = ni * pp.X + nj * pp.Y + nk * pp.Z + d;
+                double val2 = ni * Center.X + nj * Center.Y + nk * Center.Z + d;
+
+                if (val1 * val2 > 0)
+                {
+                    ni = -ni;
+                    nj = -nj;
+                    nk = -nk;
+                }
+
+                if (ni * (-CamPosition.X) + nj * (-CamPosition.Y) + nk * (-CamPosition.Z) + ni * p1.X + nj * p1.Y + nk * p1.Z < 0)
+                    DrawTriangleInternal(a, b, c);
             }
 
-            double[,] matrix = new double[2, 3];
-            matrix[0, 0] = p2.X - p1.X;
-            matrix[0, 1] = p2.Y - p1.Y;
-            matrix[0, 2] = p2.Z - p1.Z;
-            matrix[1, 0] = p3.X - p1.X;
-            matrix[1, 1] = p3.Y - p1.Y;
-            matrix[1, 2] = p3.Z - p1.Z;
-
-            double ni = matrix[0, 1] * matrix[1, 2] - matrix[0, 2] * matrix[1, 1];
-            double nj = matrix[0, 2] * matrix[1, 0] - matrix[0, 0] * matrix[1, 2];
-            double nk = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
-            double d = -(ni * p1.X + nj * p1.Y + nk * p1.Z);
-
-            Vector pp = new Vector(p1.X + ni, p1.Y + nj, p1.Z + nk);
-            double val1 = ni * pp.X + nj * pp.Y + nk * pp.Z + d;
-            double val2 = ni * Center.X + nj * Center.Y + nk * Center.Z + d;
-
-            if (val1 * val2 > 0)
-            {
-                ni = -ni;
-                nj = -nj;
-                nk = -nk;
-            }
-
-            if (ni * (-CamPosition.X) + nj * (-CamPosition.Y) + nk * (-CamPosition.Z) + ni * p1.X + nj * p1.Y + nk * p1.Z < 0)
-                DrawTriangleInternal(a, b, c);
+          
         }
 
         // Принимает на вход координаты в пространстве экрана.
@@ -369,27 +467,57 @@ namespace task9
             for (int i = 1; i < vertices.Count - 1; ++i)
                 DrawTriangleInternal(vertices[0], vertices[i], vertices[i + 1]);
         }
-
+        private void DrawPolygonInternal2(List<Vertex> vertices)
+        {
+            for (int i = 1; i < vertices.Count - 1; ++i)
+                DrawTriangleInternal2(vertices[0], vertices[i], vertices[i + 1]);
+        }
+        private void DrawTriangleInternal2(Vertex a, Vertex b, Vertex c)
+        {
+            if (Face.None != CullFace)
+            {
+                var u = b.Coordinate - a.Coordinate;
+                var v = c.Coordinate - a.Coordinate;
+                if (Face.Counterclockwise == CullFace)
+                    Swap(ref u, ref v);
+                if (Vector.AngleVect(new Vector(0, 0, 1), Vector.CrossProduct(u, v)) > Math.PI / 2)
+                    return;
+            }
+            if (a.Coordinate.Y > b.Coordinate.Y)
+                Swap(ref a, ref b);
+            if (a.Coordinate.Y > c.Coordinate.Y)
+                Swap(ref a, ref c);
+            if (b.Coordinate.Y > c.Coordinate.Y)
+                Swap(ref b, ref c);
+            for (double y = Math.Ceiling(a.Coordinate.Y); y < c.Coordinate.Y; ++y)
+            {
+                bool topHalf = y < b.Coordinate.Y;
+                var a0 = a;
+                var a1 = c;
+                var left = Interpolate(a0, a1, (y - a0.Coordinate.Y) / (a1.Coordinate.Y - a0.Coordinate.Y));
+                var b0 = topHalf ? a : b;
+                var b1 = topHalf ? b : c;
+                var right = Interpolate(b0, b1, (y - b0.Coordinate.Y) / (b1.Coordinate.Y - b0.Coordinate.Y));
+                if (left.Coordinate.X > right.Coordinate.X)
+                    Swap(ref left, ref right);
+                for (double x = Math.Ceiling(left.Coordinate.X); x < right.Coordinate.X; ++x)
+                {
+                    var point = Interpolate(left, right, (x - left.Coordinate.X) / (right.Coordinate.X - left.Coordinate.X));
+                    SetPixel((int)x, (int)y, point.Coordinate.Z, point.Color);
+                }
+            }
+        }
 
         // Принимает на вход координаты в пространстве экрана.
         private void DrawTriangleInternal(Vertex a, Vertex b, Vertex c)
         {
+
             a.Coordinate = SpaceToClip(a.Coordinate);
             a.Coordinate = ClipToScreen(a.Coordinate);
             b.Coordinate = SpaceToClip(b.Coordinate);
             b.Coordinate = ClipToScreen(b.Coordinate);
             c.Coordinate = SpaceToClip(c.Coordinate);
             c.Coordinate = ClipToScreen(c.Coordinate);
-
-            /*if (Face.None != CullFace)
-            {
-                var u = b.Coordinate - a.Coordinate;
-                var v = c.Coordinate - a.Coordinate;
-                if (Face.Counterclockwise == CullFace)
-                    Swap(ref u, ref v);
-                if (Vector.AngleBet(new Vector(0, 0, 1), Vector.CrossProduct(u, v)) > 2*Math.PI / 3)
-                    return;
-            }*/
 
             if (a.Coordinate.Y > b.Coordinate.Y)
                 Swap(ref a, ref b);
